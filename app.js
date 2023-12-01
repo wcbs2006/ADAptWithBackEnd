d9f5817a9cb2d30f55366dae3f8ccc89d442433b
Wescley Christian <108875067+wcbs2006@users.noreply.github.com>
2023-12-01 11:51:41 -0300

    Handlebars funcionando

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3333, console.log("Hello, i'm online at 3333"));
